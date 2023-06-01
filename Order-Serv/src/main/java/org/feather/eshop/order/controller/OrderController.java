422d5f99cf5e00a9914edd332026710fdd228301
feather <Brian608@163.com>
2023-06-01 15:48:45 +0800

    图文资料

--- Order-Serv/src/main/java/org/feather/eshop/order/controller/OrderController.java
+++ Order-Serv/src/main/java/org/feather/eshop/order/controller/OrderController.java
@@ -1,9 +1,13 @@
 package org.feather.eshop.order.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 /**
  * @projectName: EShop
@@ -18,11 +22,24 @@ import org.springframework.web.client.RestTemplate;
 public class OrderController {
 
     @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @Autowired
     private RestTemplate restTemplate;
 
     @GetMapping("/order/create")
     public String createOrder(Integer productId,Integer userId){
        return restTemplate.getForObject("http://Stock-Serv/stock/reduce/"+productId,String.class);
+    }
+
+    @GetMapping("/getHost")
+    public String getHost(){
+        List<ServiceInstance> instanceList = discoveryClient.getInstances("Order-Serv");
+        StringBuilder result= new StringBuilder();
+        for (ServiceInstance instance : instanceList) {
+            result.append("  ").append(instance.getHost());
+        }
+        return result.toString();
     }
 
 }
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # SpringCloudAlibaba
+## cmd 插件开发 服务同地域优先访问  https://www.jianshu.com/p/2806074468cd
